6bb618c7ddc43ac900bc412af10b1b2cf2edccfe
Michael Howitz <mh@gocept.com>
2009-10-04 16:10:49 +0000

    test field invariant

--- src/icemac/addressbook/tests/test_fields.py
+++ src/icemac/addressbook/tests/test_fields.py
@@ -2,9 +2,11 @@
 # Copyright (c) 2009 Michael Howitz
 # See also LICENSE.txt
 
-import unittest
 import icemac.addressbook.fields
+import icemac.addressbook.interfaces
+import unittest
 import zope.interface
+import zope.interface.exceptions
 import zope.schema
 
 
@@ -24,3 +26,37 @@ class TestFields(unittest.TestCase):
             [('dummy', IDummy['dummy']),
              ('dummy2', IDummy['dummy2'])],
             self.fields.getFieldsInOrder(IDummy))
+
+
+class TestField(unittest.TestCase):
+
+    def setUp(self):
+        self.field = icemac.addressbook.fields.Field()
+        self.field.title = u'my field'
+        self.field.order = 1.5
+
+    def test_invariants_no_choice(self):
+        # There is no invariant when the type is not `choice`.
+        self.field.type = u'Text'
+        errors = zope.schema.getValidationErrors(
+            icemac.addressbook.interfaces.IField, self.field)
+        self.assertEqual([], errors)
+
+    def test_invariants_choice_no_values(self):
+        # Using `choice` type requires values
+        self.field.type = u'Choice'
+        errors = zope.schema.getValidationErrors(
+            icemac.addressbook.interfaces.IField, self.field)
+        self.assertEqual(1, len(errors))
+        self.assert_(errors[0][0] is None)
+        self.assert_(
+            isinstance(errors[0][1], zope.interface.exceptions.Invalid))
+        self.assertEqual(errors[0][1].args,
+                         (u'type "choice" requires values.',))
+
+    def test_invariants_choice_with_values(self):
+        self.field.type = u'Choice'
+        self.field.values = [u'sure', u'not really']
+        errors = zope.schema.getValidationErrors(
+            icemac.addressbook.interfaces.IField, self.field)
+        self.assertEqual([], errors)
