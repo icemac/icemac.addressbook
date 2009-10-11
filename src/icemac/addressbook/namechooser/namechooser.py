6fbd8794e4e2b174c1267b12da97a62747a70429
Michael Howitz <mh@gocept.com>
2009-10-11 16:29:32 +0000

    added a name chooser which does not reuse names

---
+++ src/icemac/addressbook/namechooser/namechooser.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2009 Michael Howitz
+# See also LICENSE.txt
+
+import BTrees.Length
+import icemac.addressbook.namechooser.interfaces
+import persistent
+import zope.component
+import zope.interface
+
+
+class NameSuffix(persistent.Persistent):
+    "Storage for name suffix."
+
+    zope.interface.implements(
+        icemac.addressbook.namechooser.interfaces.INameSuffix)
+    zope.component.adapts(
+        icemac.addressbook.namechooser.interfaces.IDontReuseNames)
+
+    def __init__(self):
+        self._suffix = BTrees.Length.Length(0)
+
+    def __iadd__(self, value):
+        self._suffix.change(value)
+        return self
+
+    def __unicode__(self):
+        return unicode(self._suffix.value)
+
+
+name_suffix = zope.annotation.factory(
+    NameSuffix, key='icemac.namechooser.DontReuseNames.NameSuffix')
+
+
+class DontReuseNames(zope.container.contained.NameChooser):
+
+    zope.component.adapts(
+        icemac.addressbook.namechooser.interfaces.IDontReuseNames)
+
+    def chooseName(self, name, object):
+        container = self.context
+
+        # remove characters that checkName does not allow
+        name = unicode(name.replace('/', '-').lstrip('+@'))
+
+        if not name:
+            name = unicode(object.__class__.__name__)
+
+        dot = name.rfind('.')
+        if dot >= 0:
+            extension = name[dot:]
+            name = name[:dot]
+        else:
+            extension = ''
+
+        name_suffix = icemac.addressbook.namechooser.interfaces.INameSuffix(
+            container)
+        while True:
+            name_suffix += 1
+            n = name + u'-' + unicode(name_suffix) + extension
+            if n not in container:
+                break
+
+        # Make sure the name is valid.  We may have started with something bad.
+        self.checkName(n, object)
+
+        return n
+
---
+++ src/icemac/addressbook/namechooser/tests.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2009 gocept gmbh & co. kg
+# See also LICENSE.txt
+
+import icemac.addressbook.namechooser.namechooser
+import zope.testing.cleanup
+import zope.testing.testrunner.layer
+
+
+class NameChooserLayer(zope.testing.testrunner.layer.UnitTests):
+    """Layer for name chooser tests."""
+
+    @classmethod
+    def setUp(cls):
+        zope.component.provideAdapter(
+            zope.annotation.attribute.AttributeAnnotations)
+        zope.component.provideAdapter(
+            icemac.addressbook.namechooser.namechooser.name_suffix)
+
+    @classmethod
+    def tearDown(cls):
+        zope.testing.cleanup.tearDown()
+
+
+def test_suite():
+    suite = zope.testing.doctest.DocFileSuite('namechooser.txt')
+    suite.layer = NameChooserLayer
+    return suite
+
