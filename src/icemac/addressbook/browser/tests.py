4d42607a0160a231682795e2148957d2117eac25
Michael Howitz <mh@gocept.com>
2010-01-09 14:19:18 +0000

    Added translation to XLS export files.

--- src/icemac/addressbook/browser/tests.py
+++ src/icemac/addressbook/browser/tests.py
@@ -10,11 +10,12 @@ def test_suite():
     return icemac.addressbook.testing.FunctionalDocFileSuite(
         "browser/addressbook/addressbook.txt",
         "browser/authentication/login.txt",
+        "browser/entities/bugfix.txt",
         "browser/entities/delete_choice_value.txt",
         "browser/entities/entities.txt",
-        "browser/entities/bugfix.txt",
         "browser/entities/file.txt",
         "browser/export/export.txt",
+        "browser/export/translation.txt",
         "browser/export/userfields.txt",
         "browser/keyword/keyword.txt",
         "browser/masterdata/masterdata.txt",
