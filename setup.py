6e35b4906a7e0de3863f57eb677e8372185a3251
Michael Howitz <mh@gocept.com>
2010-01-13 20:38:29 +0000

    Back to development: 1.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import setuptools
 def read(*path_elements):
     return "\n\n" + file(os.path.join(*path_elements)).read()
 
-version = '1.0.1'
+version = '1.0.2dev'
 
 setuptools.setup(
     name='icemac.addressbook',
