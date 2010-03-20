0943336359fac464055d8970a57d814f8bdfb713
Michael Howitz <mh@gocept.com>
2010-03-20 10:40:34 +0000

    Replaced `Simple single keyword search` by `Keyword search` which allows to search for multiple keywords concatenated by `and` or `or`.

---
+++ src/icemac/addressbook/browser/search/multi_keyword.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2010 Michael Howitz
+# See also LICENSE.txt
+
+from icemac.addressbook.i18n import MessageFactory as _
+import icemac.addressbook.browser.search.base
+import icemac.addressbook.browser.search.interfaces
+import icemac.addressbook.sources
+import stabledict
+import z3c.form.field
+import zope.catalog.interfaces
+import zope.component
+import zope.interface
+import zope.schema
+
+
+class SearchView(icemac.addressbook.browser.search.base.BaseView):
+    pass
+
+
+class SearchTermConcatenationSource(
+    icemac.addressbook.sources.TitleMappingSource):
+
+    _mapping = stabledict.StableDict(
+        (('and', _(u'search-and', default=u'and')),
+         ('or', _(u'search-or', default=u'or'))))
+
+
+class ISearchForm(zope.interface.Interface):
+
+    concat = zope.schema.Choice(
+        title=_(u'search term concatenation'),
+        source=SearchTermConcatenationSource(), default='and')
+
+    keywords = zope.schema.Set(
+        title=_('keywords'), required=False,
+        value_type=zope.schema.Choice(
+            title=_('keywords'),
+            source=icemac.addressbook.sources.keyword_source))
+
+
+class SearchForm(icemac.addressbook.browser.search.base.BaseSearchForm):
+
+    interface = ISearchForm
+
+
+concat_mapping = {'and': 'all_of',
+                  'or': 'any_of',}
+
+
+class Search(icemac.addressbook.browser.search.base.BaseSearch):
+    """Search for a muliple keyword."""
+
+    zope.component.adapts(SearchView)
+
+    def search(self, concat, keywords):
+        concat = concat_mapping[concat]
+        keywords = tuple(x.title for x in keywords)
+        catalog = zope.component.getUtility(zope.catalog.interfaces.ICatalog)
+        result_set = catalog.searchResults(keywords={concat: keywords})
+        return result_set
