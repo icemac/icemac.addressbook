0b2645c7054d78890345054fac8288d7850d0f02
Michael Howitz <mh@gocept.com>
2010-08-19 16:26:48 +0000

    Preparing release 1.4.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import setuptools
 def read(*path_elements):
     return "\n\n" + file(os.path.join(*path_elements)).read()
 
-version = '1.4.0dev'
+version = '1.4.0'
 
 setuptools.setup(
     name='icemac.addressbook',
