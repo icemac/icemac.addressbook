c1149356ad9075cb4903c647a518fc4431ebd281
Michael Howitz <mh@gocept.com>
2010-09-01 19:22:36 +0000

    allow moving items in order storage more than one place (implementation is not extremly performant, but it should be fast enough, as it would be very seldom that delta != 1)

--- src/icemac/addressbook/tests/test_orderstorage.py
+++ src/icemac/addressbook/tests/test_orderstorage.py
@@ -5,11 +5,13 @@
 import icemac.addressbook.orderstorage
 import unittest
 
-
-class TestOrderStorage(unittest.TestCase):
+class BaseTestOrderStorage(unittest.TestCase):
 
     def setUp(self):
         self.storage = icemac.addressbook.orderstorage.OrderStorage()
+
+
+class TestOrderStorage(BaseTestOrderStorage):
 
     def test_add(self):
         self.assertEqual([], list(self.storage.namespaces()))
@@ -79,59 +81,81 @@ class TestOrderStorage(unittest.TestCase):
         self.storage.add('fuz', 'bar')
         self.assertRaises(ValueError, self.storage.remove, 'foo', 'bar')
 
-    def test_up(self):
-        self.storage.add('foo', 'bar')
+    def test_get__not_existing(self):
+        self.assertRaises(KeyError, self.storage.get, 'foo1', 'bar')
+
+    def test_get__existing_in_other_namespace(self):
+        self.storage.add('foo1', 'baz')
+        self.assertRaises(KeyError, self.storage.get, 'foo1', 'bar')
+
+
+class TestOrderStorageMovement(BaseTestOrderStorage):
+
+    def setUp(self):
+        super(TestOrderStorageMovement, self).setUp()
+        self.storage.add('foo1', 'bar')
         self.storage.add('foo2', 'bar')
         self.storage.add('foo3', 'bar')
         self.storage.add('foo4', 'bar')
         self.storage.add('baz', 'fuz')
-        self.assertEqual(['foo', 'foo2', 'foo3', 'foo4'],
+
+    def test_up(self):
+        self.assertEqual(['foo1', 'foo2', 'foo3', 'foo4'],
                          list(self.storage.__iter__('bar')))
         self.storage.up('foo3', 'bar')
-        self.assertEqual(['foo', 'foo3', 'foo2', 'foo4'],
+        self.assertEqual(['foo1', 'foo3', 'foo2', 'foo4'],
                          list(self.storage.__iter__('bar')))
         self.assertEqual(['baz'], list(self.storage.__iter__('fuz')))
         self.assertEqual(1, self.storage.get('foo3', 'bar'))
         self.storage.up('foo3', 'bar')
-        self.assertEqual(['foo3', 'foo', 'foo2', 'foo4'],
+        self.assertEqual(['foo3', 'foo1', 'foo2', 'foo4'],
                          list(self.storage.__iter__('bar')))
         self.assertEqual(0, self.storage.get('foo3', 'bar'))
 
     def test_up_first_element(self):
-        self.storage.add('foo', 'bar')
-        self.assertRaises(ValueError, self.storage.up, 'foo', 'bar')
+        self.assertRaises(ValueError, self.storage.up, 'foo1', 'bar')
 
     def test_down(self):
-        self.storage.add('foo', 'bar')
-        self.storage.add('foo2', 'bar')
-        self.storage.add('foo3', 'bar')
-        self.storage.add('foo4', 'bar')
-        self.storage.add('baz', 'fuz')
         self.storage.down('foo3', 'bar')
-        self.assertEqual(['foo', 'foo2', 'foo4', 'foo3'],
+        self.assertEqual(['foo1', 'foo2', 'foo4', 'foo3'],
                          list(self.storage.__iter__('bar')))
         self.assertEqual(['baz'], list(self.storage.__iter__('fuz')))
-        self.assertEqual(0, self.storage.get('foo', 'bar'))
-        self.storage.down('foo', 'bar')
-        self.assertEqual(['foo2', 'foo', 'foo4', 'foo3'],
+        self.assertEqual(0, self.storage.get('foo1', 'bar'))
+        self.storage.down('foo1', 'bar')
+        self.assertEqual(['foo2', 'foo1', 'foo4', 'foo3'],
                          list(self.storage.__iter__('bar')))
-        self.assertEqual(1, self.storage.get('foo', 'bar'))
+        self.assertEqual(1, self.storage.get('foo1', 'bar'))
 
     def test_down_last_element(self):
-        self.storage.add('foo', 'bar')
-        self.assertRaises(ValueError, self.storage.down, 'foo', 'bar')
+        self.assertRaises(ValueError, self.storage.down, 'foo4', 'bar')
 
-    def test_get__not_existing(self):
-        self.assertRaises(KeyError, self.storage.get, 'foo', 'bar')
+    def test_up_delta(self):
+        self.storage.up('foo3', 'bar', 2)
+        self.assertEqual(['foo3', 'foo1', 'foo2', 'foo4'],
+                         list(self.storage.__iter__('bar')))
 
-    def test_get__existing_in_other_namespace(self):
-        self.storage.add('foo', 'baz')
-        self.assertRaises(KeyError, self.storage.get, 'foo', 'bar')
+    def test_up_first_element_delta(self):
+        self.assertRaises(ValueError, self.storage.up, 'foo2', 'bar', 2)
+
+    def test_down_delta(self):
+        self.storage.down('foo2', 'bar', 2)
+        self.assertEqual(['foo1', 'foo3', 'foo4', 'foo2'],
+                         list(self.storage.__iter__('bar')))
+
+    def test_down_last_element_delta(self):
+        self.assertRaises(ValueError, self.storage.down, 'foo3', 'bar', 2)
+
+    def test_down_delta_0(self):
+        self.storage.down('foo2', 'bar', 0)
+        self.assertEqual(['foo1', 'foo2', 'foo3', 'foo4'],
+                         list(self.storage.__iter__('bar')))
+
+    def test_down_delta_negative(self):
+        self.storage.down('foo2', 'bar', -2)
+        self.assertEqual(['foo1', 'foo2', 'foo3', 'foo4'],
+                         list(self.storage.__iter__('bar')))
 
     def test_get(self):
-        self.storage.add('foo3', 'bar')
-        self.storage.add('foo2', 'bar')
-        self.storage.add('foo1', 'bar')
         self.storage.add('foo2', 'baz')
         self.assertEqual(1, self.storage.get('foo2', 'bar'))
         self.assertEqual(0, self.storage.get('foo2', 'baz'))
