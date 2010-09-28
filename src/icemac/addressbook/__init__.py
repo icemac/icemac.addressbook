6db51091d8bc85ecc64e7f0bd97b0895ade0f434
Michael Howitz <mh@gocept.com>
2010-09-28 18:37:58 +0000

    more doc

--- src/icemac/addressbook/__init__.py
+++ src/icemac/addressbook/__init__.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2010 Michael Howitz
+# See also LICENSE.txt
+
+# Copyright string which can be recognized by fix-copyright script:
 copyright = '''
 # Copyright (c) 2008-2010 Michael Howitz
 '''[13:-1]
