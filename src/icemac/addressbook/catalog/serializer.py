52ce51fb11c243bbfa492952967f2ee028646129
Michael Howitz <mh@gocept.com>
2011-03-17 20:39:18 +0000

    snap shot work on serialization infrastructure for full text index

---
+++ src/icemac/addressbook/catalog/serializer.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2011 Michael Howitz
+# See also LICENSE.txt
+
+def FieldSerializer(field, obj):
+    """Serializes the value of a field to a unicode value."""
+    return unicode(field.get(obj))
+
+
+def FieldNoSerializer(field, obj):
+    """Serializes the value of a field to the empty string."""
+    return u''
