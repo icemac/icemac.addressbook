685951aad5ddcd81526df3228653bbba0e308920
Michael Howitz <mh@gocept.com>
2012-01-03 13:13:47 +0000

    removed duplicate cancel button + added test for it

--- src/icemac/addressbook/browser/addressbook/edit.py
+++ src/icemac/addressbook/browser/addressbook/edit.py
@@ -25,10 +25,6 @@ class EditForm(icemac.addressbook.browser.base.GroupEditForm):
     def handleApply(self, action):
         super(EditForm, self).handleApply(self, action)
 
-    @z3c.form.button.buttonAndHandler(_('Cancel'), name='cancel')
-    def handleCancel(self, action):
-        self.status = self.noChangesMessage
-
     @z3c.form.button.buttonAndHandler(
         _(u'Delete all persons in address book'), name='delete_content',
         condition=icemac.addressbook.browser.base.can_access(
