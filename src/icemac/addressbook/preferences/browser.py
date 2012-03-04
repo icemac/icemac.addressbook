bec498aeceec922ffb375acbf5a9f046909cf26c
Michael Howitz <mh@gocept.com>
2012-03-04 16:30:14 +0000

    added toggle for prefs

--- src/icemac/addressbook/preferences/browser.py
+++ src/icemac/addressbook/preferences/browser.py
@@ -2,9 +2,18 @@
 # Copyright (c) 2010-2012 Michael Howitz
 # See also LICENSE.txt
 
-import icemac.addressbook.browser.resource
-import z3c.preference.browser
+import fanstatic
 import icemac.addressbook.browser.base
+import icemac.addressbook.browser.resource
+import js.jqueryui
+import z3c.preference.browser
+
+
+lib = fanstatic.Library('prefs', 'resources')
+css = fanstatic.Resource(
+        lib, 'prefs.css',
+        depends=[icemac.addressbook.browser.resource.form_css])
+js = fanstatic.Resource(lib, 'prefs.js', depends=[js.jqueryui.jqueryui])
 
 
 class EditForm(icemac.addressbook.browser.base.BaseEditForm,
@@ -15,5 +24,6 @@ class EditForm(icemac.addressbook.browser.base.BaseEditForm,
     __init__ = z3c.preference.browser.CategoryEditForm.__init__
 
     def update(self):
-        icemac.addressbook.browser.resource.form_css.need()
+        css.need()
+        js.need()
         super(EditForm, self).update()
