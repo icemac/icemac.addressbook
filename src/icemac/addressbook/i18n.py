4a111fed1df691dafff1166996deb21bfebe5514
Michael Howitz <mh@gocept.com>
2013-01-04 20:56:31 +0000

    ws - id

--- src/icemac/addressbook/i18n.py
+++ src/icemac/addressbook/i18n.py
@@ -1,10 +1,9 @@
-# -*- coding: latin-1 -*-
 # Copyright (c) 2008-2012 Michael Howitz
 # See also LICENSE.txt
-# $Id$
-
 """I18n message factory for icemac.addressbook. """
 
+
 import zope.i18nmessageid
+
 
 _ = MessageFactory = zope.i18nmessageid.MessageFactory('icemac.addressbook')
