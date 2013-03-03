4e772b2ab6720d2d53b76a8868b7cc4b20a22814
Michael Howitz <mh@gocept.com>
2013-03-03 13:40:38 +0000

    Made usage of SeleniumTestCase easier + refactored test to test only relevant things.

--- src/icemac/addressbook/browser/entities/tests.py
+++ src/icemac/addressbook/browser/entities/tests.py
@@ -4,30 +4,10 @@ import transaction
 
 class TestFieldOrder(icemac.addressbook.testing.SeleniumTestCase):
 
-    def setUp(self):
-        super(TestFieldOrder, self).setUp()
-        ab = self.layer['addressbook']
-        icemac.addressbook.testing.create_user(
-            ab, ab, u'Selenium', u'Tester', u'sel@example.com', '12345678',
-            ['Administrator'])
-        transaction.commit()
-
     def test_field_movement(self):
+        self.login('mgr')
         sel = self.selenium
-        sel.open('http://%s/++skin++AddressBook/ab' % self.selenium.server)
-        # Login
-        sel.type("login", "sel@example.com")
-        sel.type("password", "12345678")
-        sel.clickAndWait("SUBMIT")
-        # Open Masterdata
-        sel.clickAndWait("//ul[@id='main-menu']/li[4]/a/span")
-        sel.assertLocation(r'regexp:@@masterdata\.html$')
-        # Go to entities
-        sel.clickAndWait("//div[@id='content']/ul/li[3]/a/span")
-        sel.assertLocation(r'regexp:\+\+attribute\+\+entities$')
-        # Open fields of person entity
-        sel.clickAndWait("//div[@id='content']/table/tbody/tr[2]/td[4]/a")
-        sel.assertLocation(r'regexp:icemac\.addressbook\.person\.Person$')
+        sel.open('/ab/++attribute++entities/icemac.addressbook.person.Person')
 
         # Keywords-Field has a language depenend title, we store it here for
         # the assertion of the movement later on
