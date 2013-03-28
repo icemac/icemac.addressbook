ef0a173297bf71e8b4a85f9078b6b84191924f9e
Michael Howitz <mh@gocept.com>
2013-03-28 17:55:57 +0100

    Now displays the name of the address book in HTML title tag and as headline inside the application.

---
+++ src/icemac/addressbook/browser/layout.py
@@ -0,0 +1,15 @@
+from icemac.addressbook.interfaces import IAddressBook
+import zope.contentprovider.provider
+
+
+class AddressBookTitle(
+    zope.contentprovider.provider.ContentProviderBase):
+    """Content provider for the addressbook title string."""
+
+    default_title = u'icemac.addressbook'
+
+    def render(self):
+        address_book = IAddressBook(self.context)
+        if not IAddressBook.providedBy(address_book):
+            return self.default_title
+        return address_book.title or self.default_title
