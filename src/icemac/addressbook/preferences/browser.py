a1804f08d85b39ac38975ed1e84b528ae686e681
Michael Howitz <mh@gocept.com>
2014-03-23 14:48:52 +0100

    Highlight preferences tab in site menu when editing preferences

--- src/icemac/addressbook/preferences/browser.py
+++ src/icemac/addressbook/preferences/browser.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2010-2014 Michael Howitz
 # See also LICENSE.txt
+import grokcore.component as grok
 import icemac.addressbook.browser.base
+import icemac.addressbook.browser.interfaces
+import icemac.addressbook.browser.menus.menu
+import z3c.menu.ready2go.checker
+import z3c.menu.ready2go.item
 import z3c.preference.browser
 import zope.interface
-import icemac.addressbook.browser.interfaces
+import zope.preference.interfaces
 
 
 @zope.interface.implementer(
@@ -31,3 +36,24 @@ class PrefGroupEditForm(icemac.addressbook.browser.base.BaseEditForm,
         # Stay on form but reload the values from database in case of
         # cancel:
         self.request.response.redirect(self.request.URL)
+
+
+class PreferencesMenuItem(z3c.menu.ready2go.item.SiteMenuItem):
+    """Menu item for the preferences tab in the site menu."""
+
+
+class PreferencesMenuItemSelectedChecker(
+        z3c.menu.ready2go.checker.TrueSelectedChecker,
+        grok.MultiAdapter):
+    """Selected checker for the Preferences menu item in the site menu."""
+
+    grok.adapts(zope.interface.Interface,
+                icemac.addressbook.browser.interfaces.IAddressBookLayer,
+                zope.interface.Interface,
+                icemac.addressbook.browser.menus.menu.MainMenu,
+                PreferencesMenuItem)
+
+    @property
+    def selected(self):
+        return zope.preference.interfaces.IPreferenceGroup.providedBy(
+            self.context)
