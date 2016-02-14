f2a218bf44f874a2d11ea1caa94340576df0c087
Michael Howitz <mh@gocept.com>
2016-02-14 21:01:42 +0100

    Update to grokcore.component >= 2.7.dev1

--- src/icemac/addressbook/__init__.py
+++ src/icemac/addressbook/__init__.py
@@ -1,12 +1,1 @@
-import grokcore.component.zcml
-
-
 copyright = '''(c) 2008-2015 Michael Howitz'''
-
-
-def pytest_skip_tests(name):
-    """grokcore.component.zcml.skip_tests() adapted to py.test."""
-    return name in ['tests', 'ftests', 'testing', 'conftest']
-
-# Monkey patch until new grokcore.component version supports pytest
-grokcore.component.zcml.skip_tests = pytest_skip_tests
