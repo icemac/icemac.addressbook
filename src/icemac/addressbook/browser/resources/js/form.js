5982bdeb9d43b4ca05cdba96f1ac65c49d1c7dd2
Michael Howitz <mh@gocept.com>
2016-05-10 21:40:28 +0200

    Render field descriptions (resp. notes of user defined fields) as hint text below the widget in the form.

--- src/icemac/addressbook/browser/resources/js/form.js
+++ src/icemac/addressbook/browser/resources/js/form.js
@@ -1,4 +1,21 @@
 (function($) {
+
+    function render_field_hint(index, widget) {
+        /* Render the title attribute of a form field as hint.
+
+        widget ... z3c.form widget rendered in browser.
+        */
+        widget = $(widget);
+        var text = widget.find('input, select').attr('title');
+        if (!text) {
+            return;
+        }
+        widget.append('<div class="hint">' + text + '</div>');
+    };
+
     // Put focus on first input field of form:
     $("form div.widget:first input").focus();
+    // Render field descriptions as hints.
+    $.each($('form .widget'), render_field_hint);
+
 })(jQuery);
