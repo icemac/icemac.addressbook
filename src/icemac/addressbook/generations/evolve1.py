ae72734512ecc614336ff937b20e49d73fbffe74
Michael Howitz <mh@gocept.com>
2017-01-07 13:10:23 +0100

    Remove generations which use no longer existing functions at module level.
    This breaks i18n extract.

--- src/icemac/addressbook/generations/evolve1.py
+++ src/icemac/addressbook/generations/evolve1.py
@@ -1,28 +1,10 @@
 # -*- coding: latin-1 -*-
-import zope.generations.utility
-import icemac.addressbook.interfaces
-import icemac.addressbook.person
 import icemac.addressbook.generations.utils
 
 generation = 1
 
-person_created_with_set_site = icemac.addressbook.utils.set_site(
-    icemac.addressbook.person.person_created)
-
 
 def evolve(context):
-    """Install the necessary components for address books.
-
-    Defines references on persons.
-    """
-    root = zope.generations.utility.getRootFolder(context)
-    # persons need gocept.reference which needs a local utility stored in root
-
-    # persons
-    persons = zope.generations.utility.findObjectsProviding(
-        root, icemac.addressbook.interfaces.IPerson)
-    for person in persons:
-        person_created_with_set_site(root, person, None)
-
+    """Install the necessary components for address books."""
     icemac.addressbook.generations.utils.update_address_book_infrastructure(
         context)
