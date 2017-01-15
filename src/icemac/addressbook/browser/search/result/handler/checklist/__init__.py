2533c50a64d9cfc32236328f9bc0977b7e753d50
Michael Howitz <mh@gocept.com>
2017-01-15 15:12:39 +0100

    Add a search result handler which renders a list of the selected persons with a check-box in front.

---
+++ src/icemac/addressbook/browser/search/result/handler/checklist/__init__.py
@@ -0,0 +1,20 @@
+import icemac.addressbook.browser.base
+import icemac.addressbook.browser.menus.menu
+import icemac.addressbook.browser.search.result.handler.base
+
+
+class Checklist(icemac.addressbook.browser.search.result.handler.base.Base,
+                icemac.addressbook.browser.base.BaseView):
+    """Check-list with person names."""
+
+    def person_list(self):
+        for person in self.persons:
+            name = icemac.addressbook.interfaces.IPersonName(person).get_name()
+            yield {'name': name,
+                   'id': person.__name__}
+
+    def person_count(self):
+        return len(self.persons)
+
+checklist = icemac.addressbook.browser.menus.menu.SelectMenuItemOn(
+    'person-checklist.html')
