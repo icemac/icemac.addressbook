bbc6fcbd4c46801e51809691047d3bc7a3c76007
Michael Howitz <mh@gocept.com>
2017-05-28 19:39:01 +0200

    Bring test coverage back to 100%.

--- src/icemac/addressbook/browser/search/result/handler/tests/test_manager.py
+++ src/icemac/addressbook/browser/search/result/handler/tests/test_manager.py
@@ -24,3 +24,12 @@ def test_manager__SearchResultHandler____eq____3():
     """It is not equal to anything else."""
     # There is no __neq__ implemented!
     assert not(makeSRHandler(None) == object())
+
+
+def test_manager__SearchResultHandler____hash____1():
+    """It is hashable.
+
+    It is only needed for Python 3 where classes having an __eq__ method do
+    not have a __hash__ method.
+    """
+    assert hash(SearchResultHandler())
