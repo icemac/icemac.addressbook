b586d21aa8da03d6fa029cc761232952c04c76ca
Michael Howitz <mh@gocept.com>
2018-01-05 10:31:01 +0100

    Fix the breadcrumbs on the logout page.

--- src/icemac/addressbook/browser/authentication/authviewlet.py
+++ src/icemac/addressbook/browser/authentication/authviewlet.py
@@ -8,7 +8,25 @@ class FlashedHTTPAuthenticationLogout(
         icemac.addressbook.browser.base.FlashView):
     """HTTPAuthenticationLogout enriched by flash messages."""
 
+    title = _('logout')
+
     def logout(self, nextURL=None):
         result = super(FlashedHTTPAuthenticationLogout, self).logout(nextURL)
         self.send_flash(_('You have been logged out successfully.'))
         return result
+
+
+class LogoutRedirectPagelet(
+        z3c.authviewlet.auth.LogoutRedirectPagelet,
+        icemac.addressbook.browser.base.FlashView):
+    """Pagelet to display logout redirect."""
+
+    title = _('redirect')
+
+
+class LogoutSuccessPagelet(
+        z3c.authviewlet.auth.LogoutSuccessPagelet,
+        icemac.addressbook.browser.base.FlashView):
+    """Pagelet to display logout success."""
+
+    title = _('successful')
