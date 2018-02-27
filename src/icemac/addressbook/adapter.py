8e156a8bda5e6d5b3b8ac6def5a91a5d0eff46ce
Michael Howitz <mh@gocept.com>
2018-02-27 18:43:36 +0100

    Add an index to distinguish between the entities in the catalog.

--- src/icemac/addressbook/adapter.py
+++ src/icemac/addressbook/adapter.py
@@ -1,9 +1,12 @@
+import grokcore.component as grok
 import icemac.addressbook.interfaces
 import icemac.addressbook.principals.interfaces
 import six
 import zope.component.hooks
 import zope.interface
+import zope.interface.interfaces
 import zope.schema.interfaces
+import zope.security.proxy
 
 
 @zope.interface.implementer(icemac.addressbook.interfaces.ITitle)
@@ -32,3 +35,27 @@ def default_title(obj):
 def principal_root(principal):
     """Return the site root."""
     return zope.component.hooks.getSite()
+
+
+@grok.implementer(icemac.addressbook.interfaces.ISchemaName)
+class SchemaProvider_SchemaName(grok.Adapter):
+    """Adapter between ISchemaProvider and ISchemaName."""
+
+    grok.context(icemac.addressbook.interfaces.ISchemaProvider)
+
+    def __init__(self, context):
+        # Instead of removing the security proxy I could require a ZCML class
+        # definition of the usable interfaces. They are not writeable so it
+        # seems too much effort to require the declaration.
+        self.schema_name = unicode(
+            zope.security.proxy.getObject(context.schema).getName())
+
+
+@grok.implementer(icemac.addressbook.interfaces.ISchemaName)
+class Interface_SchemaName(grok.Adapter):
+    """Adapter between Interface and ISchemaName."""
+
+    grok.context(zope.interface.interfaces.IInterface)
+
+    def __init__(self, context):
+        self.schema_name = unicode(context.getName())
