1c213a6ed08668d9ad951aa2d2a8e24b7618e62c
Michael Howitz <mh@gocept.com>
2018-02-28 20:22:25 +0100

    Render exceptions a bit nicer: at least in the breadcrumbs.

--- src/icemac/addressbook/tests/test_adapter.py
+++ src/icemac/addressbook/tests/test_adapter.py
@@ -18,6 +18,15 @@ def test_adapter__default_title__3(zcmlS):
     assert ITitle(object()).startswith('<object object at 0x')
 
 
+def test_adapter__exception_title__1(zcmlS):
+    """It renders the exception manually instead of using str(exception).
+
+    The latter is overwritten by Chameleon with a currently ugly and lengthy
+    representation.
+    """
+    assert u'<KeyError(42,)>' == ITitle(KeyError(42))
+
+
 def test_adapter__SchemaProvider_SchemaName__schema_name__1(zcmlS):
     """It is the name of the interface of the schema provider."""
     @zope.interface.implementer(icemac.addressbook.interfaces.ISchemaProvider)
