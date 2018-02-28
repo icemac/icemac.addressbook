3df641326972d0ab65c99be20f7de54b197c82ac
Michael Howitz <mh@gocept.com>
2018-02-28 20:22:55 +0100

    Adapt to current test doc string convention.

--- src/icemac/addressbook/tests/test_adapter.py
+++ src/icemac/addressbook/tests/test_adapter.py
@@ -4,17 +4,17 @@ import zope.interface
 
 
 def test_adapter__default_title__1(zcmlS):
-    """`default_title` returns a string unchanged."""
+    """It returns a string unchanged."""
     assert 'asdf' == ITitle('asdf')
 
 
 def test_adapter__default_title__2(zcmlS):
-    """`default_title` returns a unicode unchanged."""
+    """It returns a unicode unchanged."""
     assert u'qwe' == ITitle(u'qwe')
 
 
 def test_adapter__default_title__3(zcmlS):
-    """`default_title` returns a the string representation of other objects."""
+    """It returns a the string representation of other objects."""
     assert ITitle(object()).startswith('<object object at 0x')
 
 
