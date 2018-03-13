5d571696143708dffaa4ae9d213b11795ea4f79d
Michael Howitz <mh@gocept.com>
2018-03-13 17:14:06 +0100

    Render nicer bread crumbs on a NotFound exception.

--- src/icemac/addressbook/browser/tests/test_breadcrumb.py
+++ src/icemac/addressbook/browser/tests/test_breadcrumb.py
@@ -1,5 +1,7 @@
 from ..breadcrumb import IBreadcrumb, Breadcrumb
+from zope.testbrowser.browser import HTTPError
 import mock
+import pytest
 import zope.interface.verify
 
 
@@ -19,3 +21,12 @@ def test_breadcrumb__Breadcrumb____repr____1():
     breadcrumb = MyBreadcrumb(None, None)
     assert ("<icemac.addressbook.browser.tests.test_breadcrumb.MyBreadcrumb:"
             " u'my title'>" == repr(breadcrumb))
+
+
+def test_breadcrumb__NotFoundBreadcrumb__1(address_book, browser):
+    """It renders a nice title in the breadcrumbs if a page cannot be found."""
+    browser.login('visitor')
+    with pytest.raises(HTTPError) as err:
+        browser.open('http://localhost/I-do-not-exist')
+    assert 'HTTP Error 404: Not Found' == str(err.value)
+    assert '>Not Found<' in browser.contents
