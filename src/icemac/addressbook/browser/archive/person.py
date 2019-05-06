3ad63d0e599a93102b4a0746a815fd61a1a061d0
Michael Howitz <mh@gocept.com>
2019-05-06 19:45:22 +0200

    Add view for archived person.

--- src/icemac/addressbook/browser/archive/person.py
+++ src/icemac/addressbook/browser/archive/person.py
@@ -1,6 +1,32 @@
+from icemac.addressbook.browser.person.person import DefaultSelectGroup
 import icemac.addressbook.browser.person.person
+import icemac.addressbook.browser.resource
 
 
 class ArchivedPersonForm(
         icemac.addressbook.browser.person.person.PersonEditForm):
     """View the archived person."""
+
+    title = icemac.addressbook.browser.breadcrumb.DO_NOT_SHOW
+
+    def update(self):
+        icemac.addressbook.browser.resource.bootstrap.need()
+        super(ArchivedPersonForm, self).update()
+
+    def person_data(self):
+        for group in self.groups:
+            if isinstance(group, DefaultSelectGroup):
+                continue
+            yield {
+                'label': group.label,
+                'metadata': [],
+                'data': [{
+                    'label': field.field.title,
+                    'hint': field.field.description,
+                    'value': group.widgets.get(id).render(),
+                } for id, field in group.fields.items()
+                ]
+            }
+
+    def unarchive_url(self):
+        return self.url(self.context, 'unarchive.html')
